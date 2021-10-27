67896bbb77edd8b544b71a85ef1b6403867ee23b
Josh Hernandez <whiteintheiris@gmail.com>
2021-10-27 11:45:01 -0700

    all components synced and app working woot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './styles/App.css';
-// import CheckList from './components/CheckList';
+import CheckList from './components/CheckList';
 import Header from './components/Header';
-import toDosData from './components/ToDosData';
-import ToDoItem from './components/ToDoItem';
 
 
 
-
-function App(){
-
-  const toDoComponent = toDosData.map(item => <ToDoItem key={item.id} item={item} />) 
-    
+function App(){    
   
   return(
     <div className="App">
       <Header />
-      {/* <CheckList className="CheckList" /> */}
-      {toDoComponent}
-
+      <CheckList className="CheckList" />
     </div>
 
   )
--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ToDoItem from "./ToDoItem";
+import toDosData from "./ToDosData";
+
 
 function CheckList(){
+
+      const toDoComponent = toDosData.map(item => <ToDoItem key={item.id} item={item} />) 
+
+
     return(
         <div className="CheckList container text-center">
             
             <div className="row">
                 <div className="col">
-                    <ToDoItem />
+                    {toDoComponent}
 
                 </div>
                 
